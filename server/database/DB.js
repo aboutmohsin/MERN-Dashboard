599b81e68242de53bec8169ed6c65b4d2d104d98
Mohsin Hassan <86055307+aboutmohsin@users.noreply.github.com>
2023-08-03 12:39:33 +0500

    Update DB.js

--- server/database/DB.js
+++ server/database/DB.js
@@ -2,12 +2,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const Connection = async () => {
+    console.log("Try to  Connect Database");
+  
   try {
     await mongoose.connect(process.env.DB, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
-    console.log("Database connected Successfully");
+    console.log("Database Connected Successfully");
   } catch (error) {
     console.log(`Error while connecting database: ${error}`);
   }
